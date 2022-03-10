0a2009f47a1f3da3fb41ca60677fcef25fc8f667
miguelmurillo21 <99706975+miguelmurillo21@users.noreply.github.com>
2022-03-09 19:52:55 -0500

    primer corte

---
+++ Corte_01/Semana_01/ejercicios.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Linq;
+using System.collections;
+using System.collections.generic;
+using System.text;
+namespace ejercicios
+{
+ class program 
+  {       
+  static void Main(string[] args)
+    {   
+          //ejercicio 1, Valor adsoluto
+        double a,b,c,d ; 
+        Console.WriteLine("escriba su numero");
+        a = double.parse(Console.ReadLine());
+        if (a<=0)
+         { 
+            b = a * -1; 
+            console.WriteLine("el valor adsoluto es{0} ",b)
+         }
+         else
+         {
+             console.WriteLine("el valor adsoluto  {0} ",a )
+         }
+
+
+}
+
+
+
+ }   
+
+
