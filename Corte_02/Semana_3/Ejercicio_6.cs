2b72a5eabe1510cb2d15ba1fa76f8337a78e2f95
miguelmurillo21 <99706975+miguelmurillo21@users.noreply.github.com>
2022-04-19 15:54:22 -0500

    ..

--- Corte_02/Semana_3/Ejercicio_6.cs
+++ Corte_02/Semana_3/Ejercicio_6.cs
@@ -52,4 +52,4 @@ namespace Ejercicin05
 
     }
 }
- 
+ -
---
+++ Corte_02/Semana_4/Ejercicio_13.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+
+namespace Eerciciotre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue ColaC = new Queue();
+            Stack Pila = new Stack();
+
+            ColaC.Enqueue(1.5);
+            ColaC.Enqueue(3.9);
+            ColaC.Enqueue(6.3);
+            ColaC.Enqueue(8.7);
+            ColaC.Enqueue(11.1);
+            ColaC.Enqueue(13.5);
+
+
+            Console.WriteLine("la cola inicial es: ");
+
+            while (ColaC.Count > 0)
+            {
+                double Co = 0.0F;
+                Co = (double)ColaC.Dequeue();
+                Console.WriteLine($"  Cola: {Co}");
+                Pila.Push(Co); 
+            }
+            
+            Console.WriteLine("La pila creada: ");
+            while (Pila.Count > 0)
+            {
+                Console.WriteLine($"  Pila: {Pila.Pop()}");
+            }
+        }
+    }
+}
